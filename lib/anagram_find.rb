9a17e8291314fa7f79e9f21b206091e0cf0dc610
kishorecherukuror <kishorecheruku.ror@gmail.com>
2019-04-26 04:36:33 -0700

    Find anagram

---
+++ lib/anagram_find.rb
@@ -0,0 +1,21 @@
+require "anagram_find/version"
+
+module AnagramFind
+  class Error < StandardError; end
+    class << self
+      def method_missing(m, *args, &block)
+          puts "#{m} method Does't exit"
+      end
+      def anagram(filepath,word)
+          words = []
+          begin
+              File.open(filepath,"r") { |f| f.each_line { |line| words << line.chomp.split(/[\s,-]+/) }}
+          rescue
+              puts "No such file"
+              exit
+          end
+          anagram = words.flatten.group_by {|w| w.downcase.chars.sort }.values.select {|arr| arr.include? word.downcase}.flatten
+          anagram.empty? ? "No anagram" : anagram
+      end
+    end
+end
---
+++ lib/anagram_find/version.rb
@@ -0,0 +1,3 @@
+module AnagramFind
+  VERSION = "0.1.0"
+end
